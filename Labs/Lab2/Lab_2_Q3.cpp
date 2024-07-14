d108dbb1f5032fba18f97bfb5c33b108992fa993
Fatimaa Khan <164335042+fatimazherk@users.noreply.github.com>
2024-07-13 22:56:09 -0700

    Update Lab_2_Q3.cpp
    Signed-off-by: Fatimaa Khan <164335042+fatimazherk@users.noreply.github.com>

--- Labs/Lab2/Lab_2_Q3.cpp
+++ Labs/Lab2/Lab_2_Q3.cpp
@@ -1,3 +1,12 @@
+// Create a class called Employee that includes three pieces of information as data
+// members—a first name (type char* (DMA)), a last name (type string) and a monthly salary
+// (type int). Your class should have a setter function that initializes the three data members.
+// Provide a getter function for each data member. If the monthly salary is not positive, set it
+// to 0. Write a test program that demonstrates class Employee’s capabilities. Create two
+// Employee objects and display each object’s yearly salary. Then give each Employee a 10
+// percent raise and display each Employee’s yearly salary again. Identify and add any other
+// related functions to achieve the said goal.
+    
 #include<iostream>
 #include<string>
 
